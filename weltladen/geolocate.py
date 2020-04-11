83a03f154486ad875f4b40c57e175baa472afb24
markusmo <markus.mo@gmx.net>
2020-04-11 20:10:25 +0200

    checking location and disabling shipping

--- weltladen/geolocate.py
+++ weltladen/geolocate.py
@@ -5,21 +5,26 @@ from geopy.geocoders import Nominatim
 from weltladen.models import Locations
 from weltladen.settings import SHOP_APP_LABEL, WELTLADEN_BADEN_LOCATION
 
-def checkdistance(address):
-    if not Locations.objects.exists(zip_code=address.zip_code, country=address.country):
+def checkdistance(zip_code, city, country):
+    if not Locations.objects.filter(zip_code=zip_code, country=country).exists():
         geolocator = Nominatim(SHOP_APP_LABEL+'_baden')
-        location_str = '{0} {1}, {2}'.format(address.zip_code, address.city, address.country)
-        geocode = geolocator.gecode(location_str)
+        location_str = {
+            'postalcode': zip_code,
+            'city': city,
+            'country': country
+        }
 
-        other_city = (geocode.latitude, gecode.longitude)
+        geocode = geolocator.geocode(location_str)
+
+        other_city = (geocode.latitude, geocode.longitude)
         d = distance.distance(WELTLADEN_BADEN_LOCATION, other_city).km
 
-        location = Location.objects.create(
-            zip_code=address.zip_code,
-            city=address.city,
-            country=address.country,
+        location = Locations.objects.create(
+            zip_code=zip_code,
+            city=city,
+            country=country,
             distance=round(d)
         )
         location.save()
-    location = Locations.objects.get(zip_code=address.zip_code, country=address.country)
+    location = Locations.objects.get(zip_code=zip_code, country=country)
     return location.distance
