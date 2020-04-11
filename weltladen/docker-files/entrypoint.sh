ec50e306da3ca5ae7af5ebfb1e228ee044412e21
markusmo <markus.mo@gmx.net>
2020-04-11 16:53:56 +0200

    removing

--- weltladen/docker-files/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-npm install
-#/web/manage.py initialize_shop_demo --noinput
-/web/manage.py migrate
-exec /web/manage.py runserver 0.0.0.0:9009
--- weltladen/weltladen/forms.py
+++
@@ -1,5 +0,0 @@
-from shop.forms.checkout import CustomerForm as CustomerFormBase
-
-
-class CustomerForm(CustomerFormBase):
-    field_order = ['salutation', 'first_name', 'last_name', 'email']
