de39584bab6d71715d428efaea94f22a98fa6e70
markusmo <markus.mo@gmx.net>
2020-04-27 23:35:58 +0200

    adding contact form

---
+++ weltladen/views.py
@@ -0,0 +1,28 @@
+from django.core.exceptions import NON_FIELD_ERRORS
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from weltladen.forms import ContactUsForm
+
+class ContactUsView(GenericAPIView):
+    form_class = ContactUsForm
+    template_name = 'weltladen/forms/contact-us.html'
+
+    def post(self, request, *args, **kwargs):
+        '''
+        validate and such
+        '''
+        form_data = request.data.get(self.form_class.scope_prefix, {})
+        form = self.form_class(data=form_data, instance=customer)
+        #success
+        if form.is_valid():
+            form.save(request=request)
+            response_data = {form.form_name: {
+                'success_message': _("Successfully registered yourself."),
+            }}
+            return Response(response_data, status=status.HTTP_200_OK)
+        #handle errors
+        errors = dict(form.errors)
+        if 'email' in errors:
+            errors.update({NON_FIELD_ERRORS: errors.pop('email')})
+        return Response({form.form_name: errors}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
