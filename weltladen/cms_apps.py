b47444cb507e9f3d2c3f4730ec97074c725cc4be
markusmo <markus.mo@gmx.net>
2020-06-17 19:36:16 +0200

    working categories

--- weltladen/cms_apps.py
+++ weltladen/cms_apps.py
@@ -2,26 +2,31 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
+from rest_framework.settings import api_settings
 
 from cms.apphook_pool import apphook_pool
 from cms.cms_menus import SoftRootCutter
 from menus.menu_pool import menu_pool
 from shop.cms_apphooks import CatalogListCMSApp, CatalogSearchApp, OrderApp, PasswordResetApp
+from shop.search.mixins import ProductSearchViewMixin
+from shop.views.catalog import AddToCartView, ProductListView, ProductRetrieveView
+from shop.rest.filters import CMSPagesFilterBackend
+from weltladen.serializers import ProductDetailSerializer
 
 
 class CatalogListApp(CatalogListCMSApp):
     def get_urls(self, page=None, language=None, **kwargs):
-        from shop.search.mixins import ProductSearchViewMixin
-        from shop.views.catalog import AddToCartView, ProductListView, ProductRetrieveView
-        from weltladen.serializers import ProductDetailSerializer
-
-        ProductListView = type('ProductSearchListView', (ProductSearchViewMixin, ProductListView), {})
+        
+        filter_backends = [CMSPagesFilterBackend]
+        filter_backends.extend(api_settings.DEFAULT_FILTER_BACKENDS)
+        ProductListViewClass = type('ProductSearchListView', (ProductSearchViewMixin, ProductListView), {})
         return [
             url(r'^(?P<slug>[\w-]+)/add-to-cart', AddToCartView.as_view()),
             url(r'^(?P<slug>[\w-]+)', ProductRetrieveView.as_view(
                 serializer_class=ProductDetailSerializer,
             )),
-            url(r'^', ProductListView.as_view(
+            url(r'^', ProductListViewClass.as_view(
+                filter_backends=filter_backends,
                 redirect_to_lonely_product=True,
             )),
         ]
