f3aa80cbc2b0413d87723f2384e91fd44d7ee3b1
markusmo <markus.mo@gmx.net>
2020-10-20 21:34:25 +0200

    require dotenv and read environment variables when loading wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
 import os
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
+
+#load environment
+project_folder = os.path.expanduser('~/django_project')
+load_dotenv(os.path.join(project_folder, '.env'))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weltladen.settings.develop')
 application = get_wsgi_application()
