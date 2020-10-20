8764b0d18f8976fb3dadc3377fca6fa2c5f98fb3
markusmo <markus.mo@gmx.net>
2020-10-20 22:18:03 +0200

    send mails bcc

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
+#https://help.pythonanywhere.com/pages/environment-variables-for-web-apps/
 #load environment
 project_folder = os.path.expanduser('~/django_project')
 load_dotenv(os.path.join(project_folder, '.env'))
