fedb021e106492abdf8e958a550120d60bd40878
markusmo <markus.mo@gmx.net>
2020-11-22 13:27:26 +0100

    add different tokenizer for elasticsearch

--- weltladen/cms_apps.py
+++ weltladen/cms_apps.py
@@ -2,13 +2,16 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
+from django.utils.translation import get_language_from_request
+from django.utils.translation import gettext_lazy as _
 from rest_framework.settings import api_settings
 
 from cms.apphook_pool import apphook_pool
 from cms.cms_menus import SoftRootCutter
+from cms.app_base import CMSApp
 from menus.menu_pool import menu_pool
-from shop.cms_apphooks import CatalogListCMSApp, CatalogSearchApp, OrderApp, PasswordResetApp
-from shop.search.mixins import ProductSearchViewMixin
+from shop.cms_apphooks import CatalogListCMSApp, OrderApp, PasswordResetApp
+from shop.search.mixins import ProductSearchViewMixin, SearchViewMixin
 from shop.views.catalog import AddToCartView, ProductListView, ProductRetrieveView
 from shop.rest.filters import CMSPagesFilterBackend
 from weltladen.serializers import ProductDetailSerializer
@@ -28,6 +31,49 @@ class CatalogListApp(CatalogListCMSApp):
             url(r'^', ProductListViewClass.as_view(
                 filter_backends=filter_backends,
                 redirect_to_lonely_product=True,
+            )),
+        ]
+
+class CatalogSearchViewMixin(SearchViewMixin):
+    """
+    Mixin class to be added to the ProductListView in order to create a full-text search.
+    """
+    search_fields = ['product_name', 'product_code', 'body']
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs.setdefault('label', 'search')
+        return super().get_serializer(*args, **kwargs)
+
+    def get_queryset(self):
+        language = get_language_from_request(self.request)
+        document = self.get_document(language)
+        query = self.request.GET.get('q')
+        search = document.search().source(excludes=['body'])
+        if query:
+            search = search.query('multi_match', query=query, fields=self.search_fields)
+            #otherwise only 10 products will be returned and there will be no pagination
+            search = search[0:search.count()]
+        return search.to_queryset()
+
+
+class CatalogSearchApp(CMSApp):
+    """
+    This CMS apphook shall be used to render the list view for generic search results.
+    These results are just determined by the search query and not influenced by other means,
+    such as filters and categories. Usually this `Catalog Search` app is attached to a CMS
+    page named "Search Results". That CMS page must be tagged with the ID: 'shop-search-product'.
+    """
+    name = _("Catalog Search")
+
+    def get_urls(self, page=None, language=None, **kwargs):
+        from django.conf.urls import url
+        from shop.views.catalog import ProductListView
+
+        SearchView = type('SearchView', (CatalogSearchViewMixin, ProductListView), {})
+        return [
+            url(r'^', SearchView.as_view(
+                filter_backends=[],
+                search_fields=['product_name', 'product_code', 'body']
             )),
         ]
 
--- weltladen/search_indexes.py
+++ weltladen/search_indexes.py
@@ -1,5 +1,7 @@
 from django.conf import settings as config
 from shop.search.documents import ProductDocument
+from elasticsearch_dsl.analysis import analyzer, token_filter, tokenizer
+
 
 settings = {
     'number_of_shards': 1,
@@ -7,4 +9,19 @@ settings = {
 }
 
 for language, _ in config.LANGUAGES:
-    ProductDocument(language=language, settings=settings)
+    analyzer_name = language+'_'+_+ '_analyzer'
+    language_analizers = {
+        language: analyzer('german_analyzer',
+            type='custom',
+            tokenizer=tokenizer('trigram', 'ngram', min_gram=3, max_gram=3),
+            filter=[
+                'lowercase',
+                token_filter('asciifolding', type='asciifolding', preserve_original=False),
+                token_filter('german_stop', type='stop', language='german'),
+                token_filter('german_stemmer', type='snowball', language='german'),
+            ],
+            char_filter=['html_strip'],
+        )
+    }
+    
+    ProductDocument(language=language, settings=settings, language_analizers=language_analizers)
